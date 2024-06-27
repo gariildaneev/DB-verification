aff8776772bf76048c5dc067dd8fb2a53f3d8add
gariildaneev <daniil.gareev@gmail.com>
2024-06-27 20:16:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,15 @@ OPTIONS = {
     'argv_emulation': True,
     'packages': ['pandas', 'openpyxl'],
     'iconfile': 'assets/icon.icns',
+    'plist': {
+        'CFBundleShortVersionString': '0.1.0',
+        'LSUIElement': True,
+    },
 }
 
 setup(
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
-    setup_requires=['py2app'],
+    setup_requires=['py2app', 'wheel'],
 )
