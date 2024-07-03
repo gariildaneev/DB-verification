888c47d1193f1346112b98ba062efb46a3cb9ff9
gariildaneev <daniil.gareev@gmail.com>
2024-07-03 12:44:09 +0300

    Update comparer.py

--- comparer.py
+++ comparer.py
@@ -4,40 +4,49 @@ import xlsxwriter
 def compare_reports(file1, file2, output_file):
     df1 = pd.read_excel(file1)
     df2 = pd.read_excel(file2)
-
+    
     if 'KKS' not in df1.columns or 'KKS' not in df2.columns:
         raise ValueError("Оба файла должны содержать колонку 'KKS'")
-
+        
     df1.set_index('KKS', inplace=True)
     df2.set_index('KKS', inplace=True)
 
+
+    common_kks = df1.index.intersection(df2.index)
+
     changes = []
 
-    for kks in df1.index:
-        if kks in df2.index:
-            row1 = df1.loc[kks]
-            row2 = df2.loc[kks]
-            row_changes = {'KKS': kks}
+    for kks in common_kks:
+        row1 = df1.loc[kks]
+        row2 = df2.loc[kks]
+
+        diff = (row1 != row2)
+        if diff.any():
+            change = { 'KKS': kks }
             for col in df1.columns:
-                if col in df2.columns and not row1[col].equals(row2[col]):
-                    row_changes[col] = row1[col]
-                    row_changes[f"{col}_new"] = row2[col]
-            if len(row_changes) > 1:
-                changes.append(row_changes)
+                if diff[col]:
+                    change[col] = row1[col]
+                    change[f'{col}_new'] = row2[col]
+                else:
+                    change[col] = row1[col]
+            changes.append(change)
 
     changes_df = pd.DataFrame(changes)
 
-    with xlsxwriter.Workbook(output_file, {'nan_inf_to_errors': True}) as workbook:
-        ws_changes = workbook.add_worksheet("Отчет о изменениях")
+    workbook = xlsxwriter.Workbook(output_file)
+    ws = workbook.add_worksheet("Сравнение отчетов")
 
-        for c_idx, column in enumerate(changes_df.columns):
-            ws_changes.write(0, c_idx, column)
+    for c_idx, col in enumerate(changes_df.columns, start=0):
+        ws.write(0, c_idx, col)
 
-        for r_idx, row in enumerate(changes_df.itertuples(), start=1):
-            for c_idx, value in enumerate(row[1:], start=0):
-                if pd.isna(value) or value in [float('nan'), float('inf'), float('-inf')]:
-                    ws_changes.write(r_idx, c_idx, "")
-                elif isinstance(value, (int, float)):
-                    ws_changes.write_number(r_idx, c_idx, value)
-                else:
-                    ws_changes.write(r_idx, c_idx, str(value))
+    for r_idx, row in enumerate(changes_df.itertuples(), start=1):
+        for c_idx, value in enumerate(row[1:], start=0):
+            if pd.isna(value) or value in [float('nan'), float('inf'), float('-inf')]:
+                ws.write(r_idx, c_idx, "")
+            elif isinstance(value, (int, float)):
+                ws.write_number(r_idx, c_idx, value)
+            else:
+                ws.write(r_idx, c_idx, str(value))
+
+    workbook.close()
+
