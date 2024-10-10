9b6db00f28b6f8d292ad1e1a35304fad3ad209e3
gariildaneev <daniil.gareev@gmail.com>
2024-10-10 12:37:31 +0300

    Update main_logic.py

--- initial_distribution/main_logic.py
+++ initial_distribution/main_logic.py
@@ -1,3 +1,5 @@
+from distribution_utils import write_values, write_module_headers, handle_module_overflow, fa_wise_distribution
+
 # Process DI and DO values
 def process_discrete_values(db1, conn_diagram, num_DI, num_DO, max_modules, worksheet, workbook, current_row, current_col, current_module, current_section, sections_per_cabinet, cabinet_num):
     db1.FA.astype(int)
